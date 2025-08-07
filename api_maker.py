7d646febecf66ae61048b47cee5a305705e2ed7d
felipesanar <felipe.souza@sanar.com>
2025-08-07 17:08:25 +0000

    fix port

--- api_maker.py
+++ api_maker.py
@@ -616,4 +616,6 @@ if __name__ == '__main__':
     processar_arquivos(pasta_arquivos)
     
     # Iniciar API
-    app.run(debug=True, port=5000)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
+
